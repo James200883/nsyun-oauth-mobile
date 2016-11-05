d525c96dd0c1f40cb0d5b574cdcd5af62a2548f8
James200883 <james984557346@gmail.com>
2016-11-05 16:25:26 +0800

    1 	modified:   config.xml 	modified:   src/commons/constants/Keys.ts

--- src/commons/constants/Keys.ts
+++ src/commons/constants/Keys.ts
@@ -3,7 +3,7 @@ import {Headers} from '@angular/http';
 export class Keys {
   static USER_INFO_KEY: string = "user_info_token";
 
-  static SERVICE_URL: string = 'http://139.129.202.208:9280/nsyun-oauth-server';
+  static SERVICE_URL: string = 'http://www.yoxyc.com:9280/nsyun-oauth-server';
 
   static USER_CART_KEY:string ='user_cart';
 
